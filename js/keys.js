13f79ea241032fa437b45ef7c72733f99916f930
Eric Stanulis <erstanulis@gmail.com>
2017-10-05 16:00:14 -0500

    Did horrifying things to arrays.

--- js/keys.js
+++ js/keys.js
@@ -1,10 +1,12 @@
 console.log('this is loaded');
 
-var twitterKeys = {
+var keys = {
   consumer_key: 'cOu8eU7fhitegJomPbOMnGCpA',
   consumer_secret: 'a2BotrueLz59rIe3Zj8iZpwr3YpLmnt9PZrq00WX3mt9MK68va',
   access_token_key: '911771408212119553-6nk5rHWWsSVuXjV8tAwubHgFM2EsQDI',
   access_token_secret: 'EiPSg2937q154aGoXdhbRYL8FC43h5eWL3h4rNdsPBg05',
+  spotifyId: "59f0b5797ead479ea7d220778ecd7f2f",
+  spotifySecret: "f29cf762dc0842568de0e18185c48658"
 }
 
-module.exports = twitterKeys;
+module.exports = keys;
--- js/liri.js
+++ js/liri.js
@@ -3,6 +3,8 @@
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require("fs");
  
 
 
@@ -13,6 +15,8 @@ var consumerKey = (keys.consumer_key);
 var consumerSecret = keys.consumer_secret;
 var accessTokenKey = keys.access_token_key;
 var accessTokenSecret = keys.access_token_secret;
+var spotifyId = keys.spotifyId;
+var spotifySecret = keys.spotifySecret;
 
 var client = new Twitter({
   consumer_key: consumerKey,
@@ -27,102 +31,97 @@ spotify-this-song
 movie-this
 do-what-it-says*/
 
-var inputCommand = process.argv[2];
+var inputCommand = process.argv;
 
-switch (inputCommand){
-    case "my-tweets":
-/*node liri.js my-tweets
-This will show your last 20 tweets and when they were created at in your terminal/bash window.
-    
-twitter api
-Need .text and .created_at
-Parameters: count=20*/
-        var params = {user_id: "TestyStudent", count: 20}
-        client.get("https://api.twitter.com/1.1/statuses/user_timeline.json", params, function(error, tweets, response){
-            /*for (var i = 0; i < 20; i++) {*/
-                for (var i = 0; i < tweets.length; i++) {
-                    console.log("")//I could do /nl if I wanted to. But I don't.
-                    console.log(tweets[i].text);
-                    console.log(tweets[i].created_at);
+function uglySwitchStatement(inputCommand){
+    switch (inputCommand[2]){
+        case "my-tweets":
+            var params = {user_id: "TestyStudent", count: 20}
+            client.get("https://api.twitter.com/1.1/statuses/user_timeline.json", params, function(error, tweets, response){
+                /*for (var i = 0; i < 20; i++) {*/
+                    for (var i = 0; i < tweets.length; i++) {
+                        console.log("")//I could do /nl if I wanted to. But I don't.
+                        console.log(tweets[i].text);
+                        console.log(tweets[i].created_at);
 
-                };
-            /*}*/
-        })
+                    };
+                /*}*/
+            })
 
-        break;
-    case "spotify-this-song":
+            break;
+        case "spotify-this-song":
 
-        var spotify = new Spotify({
-            //swiper no swiping!
-            id: "59f0b5797ead479ea7d220778ecd7f2f",
-            secret: "f29cf762dc0842568de0e18185c48658"
-        });
-        var query = "";
-        if(process.argv[3] === undefined){
-            query = "The Sign Ace of Base";
-        }
-        for (var i = 3; i < process.argv.length; i++) {
-            query = query + process.argv[i] + " "
-        }
-        spotify.search({ type: 'track', query: query }, function(err, data) {
-            if (err) {
-            return console.log('Error occurred: ' + err);
-          }
+            var spotify = new Spotify({
+                //swiper no swiping!
+                id: spotifyId,
+                secret: spotifySecret
+            });
+            var query = "";
+            if(inputCommand[3] === undefined){
+                query = "The Sign Ace of Base";
+            }
+            for (var i = 3; i < inputCommand.length; i++) {
+                query = query + inputCommand[i] + " "
+            }
+            spotify.search({ type: 'track', query: query }, function(err, data) {
+                if (err) {
+                return console.log('Error occurred: ' + err);
+              }
 
-        console.log(""); //because no one has time for /nl
-        console.log("Artist: " + data.tracks.items[0].artists[0].name); 
-        console.log("Song Name: " + data.tracks.items[0].name)
-        console.log("Preview URL: " + data.tracks.items[0].external_urls.spotify)
-        console.log("Album: " + data.tracks.items[0].album.name)
-        });
+            console.log(""); //because no one has time for /nl
+            console.log("Artist: " + data.tracks.items[0].artists[0].name); 
+            console.log("Song Name: " + data.tracks.items[0].name)
+            console.log("Preview URL: " + data.tracks.items[0].external_urls.spotify)
+            console.log("Album: " + data.tracks.items[0].album.name)
+            });
 
+            break;
+        case "movie-this":
+            //I originally tried to be clever and just do var query;
+            //Unfortunately, if you try to take an undefind variable and concatenate it with a string, it becomes a string that starts with "undefined".
+            //So I'll be a good person and initalize this as a string.
+            var query = "";
+            for (i=3; i < inputCommand.length; i++){
+                query = query + inputCommand[i] + " ";
+            }
+            if (query === ""){
+                query = "Mr. Nobody";
+            }
+            //I'm not obfuscating the key because it's in the instructions and I don't care that much. Assume I threw it in the /keys.js file like all the rest of them.
+            request("http://www.omdbapi.com/?apikey=40e9cece&t=" + query, function(err, response, body){
+                //Because I want to work with a pretty result, rather than something that looks like I did "view source" on google.com.
+                var movie = JSON.parse(body);
+                //Fine. I'll use a new line in this one. In fact, I shall use ALL the new lines! Mwahaha!
+                //Mostly just to prove that I can do it.
+                //Realistically, this would be annoying to maintain and, unless there's some performance increase I'm not expecting from running one console.log instead of like 8 I'd just do 8 console.logs normally.
+                console.log("\nMovie Title: " + movie.Title + "\nMovie Year: " + movie.Year + "\nIMDB Rating: " + movie.imdbRating + "\n" + movie.Ratings[1].Source + " rating: " + movie.Ratings[1].Value + "\nCountry: " + movie.Country + "\nLanguage: " + movie.Language + "\nPlot: " + movie.Plot + "\nActors: " + movie.Actors);
 
-/*Artist(s)
-The song's name
-A preview link of the song from Spotify
-The album that the song is from
+            });
 
+            break;
+        case "do-what-it-says":
+            fs.readFile("../random.txt", "utf8", function(error, data) {
+              if (error) {
+                return console.log(error);
+              }
+              console.log(data);
+              var dataArr = data.split(",");
+              //Don't judge me for the next line.
+              dataArr.unshift("","")
+              console.log(dataArr);
+              //figure out htf to get the data in the text file to act like process.argv[2] etc
+              uglySwitchStatement(dataArr)
+              });
 
-If no song is provided then your program will default to "The Sign" by Ace of Base.
-You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-Step One: Visit https://developer.spotify.com/my-applications/#!/
-Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package. See the 
+        /*node liri.js do-what-it-says
+    Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+    It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+    Feel free to change the text in that document to test out the feature for other commands.*/
+    };
 
-
-*/
-        break;
-    case "movie-this":
-/*node liri.js movie-this '<movie name here>'
-This will output the following information to your terminal/bash window:
-
-   * Title of the movie.
-   * Year the movie came out.
-   * IMDB Rating of the movie.
-   * Rotten Tomatoes Rating of the movie.
-   * Country where the movie was produced.
-   * Language of the movie.
-   * Plot of the movie.
-   * Actors in the movie.
-
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-It's on Netflix!
-You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use 40e9cece.*/
-        break;
-    case "do-what-it-says":
-    /*node liri.js do-what-it-says
-Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-Feel free to change the text in that document to test out the feature for other commands.*/
 };
 
-
-
-
-
+uglySwitchStatement(inputCommand)
 
 
 
